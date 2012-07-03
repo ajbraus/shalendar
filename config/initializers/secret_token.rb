c956e7ea245fba25e64fc9b28b6379f181becfd1
Fenchel <msfenchel@gmail.com>
2012-07-03 10:31:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shalendar::Application.config.secret_token = 'fc5d5ac68a445f888d59e3a60e713a9979bc2e5c6178a367f3932c20d701767a5e43ebf3549a5800be37a6362e2c0a82f188b26ecbfb9df3a07b6b38bdd4eb23'
