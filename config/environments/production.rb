ae766208e4fe2679b22f775b18e8c81548fa0141
Fenchel <msfenchel@gmail.com>
2012-08-08 14:43:18 -0500

    configure email for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,19 @@ Shalendar::Application.configure do
   config.assets.compile = false
 
   config.action_mailer.default_url_options = { :host => 'calenshare.herokuapp.com' }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "calenshare.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # Generate digests for assets URLs
   config.assets.digest = true
 
