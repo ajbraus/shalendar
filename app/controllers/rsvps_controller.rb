272bbce2202b62141384d63307ee5e7a87a53f02
Fenchel <msfenchel@gmail.com>
2012-08-15 15:03:41 -0500

    prepare for merge to master

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,26 +1,23 @@
 class RsvpsController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def create
-    @mobile_user = User.find_by_id(3)
     @event = Event.find(params[:rsvp][:plan_id])
-    @mobile_user.rsvp!(@event) #used to be current_user
-    
+    current_user.rsvp!(@event)
     if @event.guests.count == @event.min
       Notifier.event_tipped(@event).deliver
     end
     redirect_to @event
 
     # respond_to do |format|
-    # 	format.html { redirect_to @event }
-    # 	format.js
+    #   format.html { redirect_to @event }
+    #   format.js
     # end
   end
 
   def destroy
-    @mobile_user = User.find_by_id(3)
     @event = Rsvp.find(params[:id]).plan
-    @mobile_user.unrsvp!(@event)
+    current_user.unrsvp!(@event)
     redirect_to @event
 
     # respond_to do |format|
