bad640abb61a2db30368d2b6587a8f49e9b0d039
Fenchel <msfenchel@gmail.com>
2012-09-06 14:02:50 -0500

    get rid of gcm migration with conflict to user migration

--- db/migrate/20120904221619_add_gcm_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddGcmToUser < ActiveRecord::Migration
-  def change
-  	add_column :users, :GCMdevice_id, :integer, :default => 0
-  	add_column :users, :GCMregistration_id, :integer, :default => 0
-  	add_column :users, :android_user, :bool, :default => false
-  end
-end
