cc0a5f9abfc723c1c44c70fb4244b5ef185dc6ea
Fenchel <msfenchel@gmail.com>
2012-09-17 17:25:56 -0500

    try notify from airbrake

--- app/models/mailer_callback.rb
+++ app/models/mailer_callback.rb
@@ -11,7 +11,11 @@ class MailerCallback
   # resque callback method
   def self.perform(mailer, email_type, *args)
   	#logger.info("Got to perform the mailer callback with mailer = #{mailer} and event #{args[0]} and guest #{args[1]} ")
+    
     mailer.constantize.send(email_type, args).deliver
+
+	rescue => ex
+	  Airbrake.notify(ex)
   end
 end
 
