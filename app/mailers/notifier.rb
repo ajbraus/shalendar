85f1cbbfc5aa7e6fa2d1a9f3ee7ab0912c999fd4
Fenchel <msfenchel@gmail.com>
2012-09-17 19:54:09 -0500

    fix syntax error

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -95,7 +95,7 @@ class Notifier < ActionMailer::Base
     @event = event
     @user= user
 
-    mail to: "msfenchel@gmail.com", from: "info@hoos.in" subject: "Your plan has changed start time."
+    mail to: "msfenchel@gmail.com", from: "info@hoos.in", subject: "Your plan has changed start time."
 
     rescue => ex
     Airbrake.notify(ex)
