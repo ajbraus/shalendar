4cf0a0960176f61d65fe1e1372b7501584b7f840
Fenchel <msfenchel@gmail.com>
2012-09-25 17:55:05 -0500

    fix migrations

--- db/migrate/20120920021943_create_invitations.rb
+++ db/migrate/20120920021943_create_invitations.rb
@@ -17,6 +17,9 @@ class CreateInvitations < ActiveRecord::Migration
 
     add_column :users, :new_invited_events_count, :integer, default: 0
 
+    add_column :rsvps, :invite_all_friends, :boolean, default: false
+    #go through and turn all current follows into friends
+
     Event.all.each do |e|
       if e.visibility == "friends_of_friends"
         e.user.invite_all_friends!(e)
@@ -28,6 +31,13 @@ class CreateInvitations < ActiveRecord::Migration
         e.guests_can_invite_friends = false
       end
     end
+    Rsvp.all.each do |r|
+      if r.plan.guests_can_invite_friends?
+        r.invite_all_friends = true
+        r.save
+      end
+    end
+    
     remove_column :events, :visibility
 
   end
