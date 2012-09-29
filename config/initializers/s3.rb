5b535dc0420eec9ab3213dde62eb13566e0078ae
Fenchel <msfenchel@gmail.com>
2012-09-29 18:03:06 -0500

    fix AWS config stuff

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,7 @@
 if Rails.env == "production" 
-   S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET_NAME']} 
+   #S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET_NAME']} 
+ 	 S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => ENV['AWS_BUCKET']} 
+
  else 
    S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
