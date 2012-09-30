57e60470b89091d41e1ef10a6bbe4b19d84725f9
Fenchel <msfenchel@gmail.com>
2012-09-30 14:54:44 -0500

    working.. trying conditional

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -53,11 +53,11 @@ module UsersHelper
 
   def fb_picture(user)
     facebook_url = "https://s3.amazonaws.com/hoosin-production/user/avatars/original/default_profile_pic.png"
-  	# if user.authentications.find_by_provider("Facebook").nil?
-   #    facebook_url = "https://s3.amazonaws.com/hoosin-production/user/avatars/original/default_profile_pic.png"
-   #  else
-   #    facebook_url = "#{user.authentications.find_by_provider("Facebook").pic_url}"
-   #  end
+  	if user.authentications.find_by_provider("Facebook").nil?
+      facebook_url = "https://s3.amazonaws.com/hoosin-production/user/avatars/original/default_profile_pic.png"
+    else
+      facebook_url = "#{user.authentications.find_by_provider("Facebook").pic_url}"
+    end
     image_tag(facebook_url, alt: user.name, class: "profile_picture" )
   end
 
