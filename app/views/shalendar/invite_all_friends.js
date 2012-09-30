cf9669b0be82e9dd445b15a396d192b98ad735b4
ajbraus <ajbraus@gmail.com>
2012-09-29 19:23:18 -0500

    deleted

--- app/views/shalendar/invite_all_friends.js
+++
@@ -1,7 +0,0 @@
-var event = $('.event').withEventId(<%= @event.id %>)
-
-event.children('.rsvp_check').children().children().html("<i class='icon icon-ok-circle'></i>").removeClass("btn_invite_all_friends").addClass("going");
-event.children('a').children('.guest_count').children('i').removeClass('purple').addClass('gold');
-event.children('.rsvp_check').children('a').click(function(e){
-	e.stopPropigation();
-});
