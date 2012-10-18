7f93e2839873cd3304e81c60b8329e6700a4aa8b
ajbraus <ajbraus@gmail.com>
2012-10-18 13:11:19 -0500

    deleted suggestion lat lng column name change

--- db/migrate/20121018154635_change_name_of_columns_suggestions.rb
+++
@@ -1,7 +0,0 @@
-class ChangeNameOfColumnsSuggestions < ActiveRecord::Migration
-  def change
-  	rename_column :suggestions, :lat, :latitude
-  	rename_column :suggestions, :lng, :longitude
-  end
-
-end
