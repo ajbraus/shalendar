15fbccdd1dd167bb9c5cdd3224651f18da075d44
ajbraus <ajbraus@gmail.com>
2012-10-31 00:04:33 -0500

    fixing edit

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -55,11 +55,6 @@ $('.datePickerDay').click(function(){
 // NEW IDEA CHECKBOX SWTICHES
 
 $("input[type=checkbox].switch").each(function() {
-  $(this).before(
-      '<span class="switch">' +
-      '<span class="mask" /><span class="background" />' +
-      '</span>'
-    );
   $(this).hide();
     // Set inital state
   if (!$(this)[0].checked) {
