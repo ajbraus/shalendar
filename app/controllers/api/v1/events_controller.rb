1f5dfef10bace7b8d92731e7891b4df8a9af23e3
Fenchel <msfenchel@gmail.com>
2012-11-05 12:52:12 -0600

    add event cancel to mobile

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -195,6 +195,23 @@ class Api::V1::EventsController < ApplicationController
     end
   end
 
+  def cancel_idea
+
+    @event = Event.find_by_id(params[:event_id])
+    @mobile_user = User.find_by_id(params[:user_id])
+    if @mobile_user.nil?
+      render :status=>400, :json=>{:error => "user was not found."}
+    end
+    if @event.nil?
+      render :status=>400, :json=>{:error => "event was not found."}
+    end
+    if @mobile_user != @event.user
+      render :status=>300, :json=>{:error => "you don't have the authority to cancel this event"}
+    end
+    Notifier.delay.cancellation(@event)
+        render :json=> { :success => true
+        }, :status=>200
+  end
   # def add_photo
   #   @mobile_user = User.find_by_id(params[:user_id])
   #   @event = Event.find_by_id(params[:event_id])
