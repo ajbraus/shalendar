cac6637e1070daee54b7e605f6ad9cee0db0a5a6
ajbraus <ajbraus@gmail.com>
2012-11-09 11:01:58 -0600

    mobile homepage

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -189,41 +189,6 @@ $(document).bind('click', function(e) {
 // });
 // });
 
-// INSTAGRAM HASHTAG FEED
-
-$(function(){
-  var
-    insta_container = $(".instagram")
-  , insta_next_url
-
-  insta_container.instagram({
-      hash: 'hoosin'
-    , clientId : '4b92d403b8324f2294a4aa3b7e2bf407'
-    , show : 6
-    , onComplete : function (photos, data) {
-      insta_next_url = data.pagination.next_url
-    }
-  })
-
-  $('#instaButton').on('click', function(){
-    var 
-      button = $(this)
-    , text = button.text()
-
-    if (button.text() != 'Loading…'){
-      button.text('Loading…')
-      insta_container.instagram({
-          next_url : insta_next_url
-        , show : 6
-        , onComplete : function(photos, data) {
-          insta_next_url = data.pagination.next_url
-          button.text(text)
-        }
-      })
-    }		
-  }) 
-});
-
 
 //FORM JS
 
