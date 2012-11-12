04729726c7f4f1fa06550820b91a4c191fdb118b
ajbraus <ajbraus@gmail.com>
2012-11-12 14:17:22 -0600

    fixing confirm follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -111,11 +111,6 @@ before_filter :authenticate_user!
       @reverse_relationship.follower.add_invitations_from_user(@relationship.followed)
     end
     respond_to do |format|
-      @friendships = current_user.reverse_relationships.where('relationships.confirmed = true')
-      #@forecastevents = current_user.forecast(Time.now.in_time_zone(current_user.time_zone))
-      #@date = Time.now.in_time_zone(current_user.time_zone)
-      @friend_requests = current_user.reverse_relationships.where('relationships.confirmed = false')
-      @vendor_friendships = current_user.vendor_friendships
       format.html { redirect_to :back, notice: "You are now friends with #{@relationship.follower.name} on hoos.in" }
       format.js
     end
