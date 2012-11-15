2defe57c3cc1c38b67daf2effe4626a119650da0
Fenchel <msfenchel@gmail.com>
2012-11-14 18:55:02 -0600

    new notifier, ..

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,22 +25,21 @@ private
       session[:graph] = Koala::Facebook::API.new(session[:access_token]) 
       @uid = env["omniauth.auth"].uid
       @user.authentications.find_by_uid(@uid).token = session[:access_token]
-
       @city = env["omniauth.auth"].info.location
-        unless City.find_by_name(@city)
-          c = City.new(name: @city)
-          c.save
-        end
+      unless City.find_by_name(@city) #we add their city, but don't auto-change
+        c = City.new(name: @city, timezone: timezone_for_utc_offset(access_token.extra.raw_info.timezone)
+        c.save
+      end
 
       # session["devise.#{kind.downcase}_data"] = env["omniauth.auth"]
       sign_in_and_redirect @user, :event => :authentication
     else
       redirect_to :back, notice: 'There was an error with Facebook. Check your Facebook account status.'
-    end   
+    end
   end
 
   def find_for_oauth(provider, access_token, resource=nil)
-    user, email, name, uid, auth_attr = nil, nil, nil, {}
+    user, email, name, uid, auth_attr = nil, nil, nil, nil, {}
     case provider
     when "Facebook"
       uid = access_token.uid
@@ -101,7 +100,14 @@ private
       email = access_token.info.email
       name = access_token.info.name
       location = access_token.info.location
-      time_zone = "Central Time (US & Canada)" #timezone_for_utc_offset(access_token.extra.raw_info.timezone)
+      @city = City.find_by_name(location)
+      if @city.nil?
+        c = City.new(name: location, timezone: timezone_for_utc_offset(access_token.extra.raw_info.timezone)
+        c.save
+        @city = c
+      end
+
+      # time_zone = "Central Time (US & Canada)" #timezone_for_utc_offset(access_token.extra.raw_info.timezone)
 
       user_attr = { email: email, name: name, city: location, time_zone: time_zone }
       user.update_attributes user_attr
---
+++ db/migrate/20121113175027_add_timezone_to_cities.rb
@@ -0,0 +1,6 @@
+class AddTimezoneToCities < ActiveRecord::Migration
+  def change
+    add_column :cities, :timezone, :string
+
+    end
+end
