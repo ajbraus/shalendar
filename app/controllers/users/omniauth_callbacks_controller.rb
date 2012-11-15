3a4b3a906ca42478648c09557fc061e0cc0aafa2
Fenchel <msfenchel@gmail.com>
2012-11-14 19:04:34 -0600

    bug fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,7 +27,7 @@ private
       @user.authentications.find_by_uid(@uid).token = session[:access_token]
       @city = env["omniauth.auth"].info.location
       if City.find_by_name(@city).nil? #we add their city, but don't auto-change
-        c = City.new(name: @city, timezone: timezone_for_utc_offset(access_token.extra.raw_info.timezone))
+        c = City.new(name: @city, timezone: "Central Time (US & Canada)")#timezone_for_utc_offset(access_token.extra.raw_info.timezone))
         c.save
       end
 
