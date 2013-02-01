a90262b423846bdf3707fa578f5c5229d3dd9aaa
ajbraus <ajbraus@gmail.com>
2013-02-01 05:56:38 -0600

    customized tooltip

--- app/assets/javascripts/littletip-1.4.js
+++ app/assets/javascripts/littletip-1.4.js
@@ -9,12 +9,12 @@
  */
 
 	// Configurable options (change value numbers to fit your needs)
-	littleTIPdelay			=   0    ;	// Delay time before appear (in pixels)
+	littleTIPdelay			=   1400    ;	// Delay time before appear (in pixels)
 	littleTIPfade			=   70     ;	// Fading duration time (in pixels)
-	littleTIPbackground		=   0.7     ;	// Background opacity (from 0 to 1)
+	littleTIPbackground		=   1     ;	// Background opacity (from 0 to 1)
 	littleTIPtext			=   1       ;	// Text opacity (from 0 to 1)
-	littleTIPx				=   -30      ;	// Horizontal distance from cursor (in pixels)
-	littleTIPy				=   0     ;	// Vertical distance from cursor (in pixels)
+	littleTIPx				=   -40      ;	// Horizontal distance from cursor (in pixels)
+	littleTIPy				=   -40     ;	// Vertical distance from cursor (in pixels)
 
 	
 
@@ -25,7 +25,7 @@ $(document).ready(function(){			// Script executed on DOM load
 	else {
 	var elT = $("[title]");			// Store all title attributes in a variable for further usage
 
-	elT.mouseover(function(){					// Function applied to title attribute on mouse over
+	elT.mouseover(function(e){					// Function applied to title attribute on mouse over
 		var elemlT = $(this).attr('title');		// Store title information in a variable for further usage
 		this.tip = this.title;					// Copy and expand littleTIP
 		this.title = "";						// Remove default tooltip
@@ -36,7 +36,9 @@ $(document).ready(function(){			// Script executed on DOM load
 			position: 'absolute',
 			display: 'none',
 			width: 'auto',
-			height: 'auto'
+			height: 'auto',
+			top: e.pageY + littleTIPy,
+			left: e.pageX + littleTIPx
 		});
 		$('#littleTIP #littleTIPbackground').css({		// littleTIP background css properties
 			zIndex: 10000001,
@@ -60,32 +62,32 @@ $(document).ready(function(){			// Script executed on DOM load
 
 
 
-	elT.mousemove(function(e){							// Function applied when mouse moves
-		var BwiWi = $(window).width();					// Store window width in a variable for further usage
-		var BwiHe = $(window).height();					// Store window height in a variable for further usage
-		var BwHs = $(window).scrollLeft();				// Store window horizontal space in a variable for further usage
-		var BwVs = $(window).scrollTop();				// Store window vertical space in a variable for further usage
-		var liTIP = $('#littleTIP');					// Store littleTIP object in a variable for further usage
-		var lTwi = $('#littleTIP').width();				// Store littleTIP width in a variable for further usage
-		var lThe = $('#littleTIP').height();			// Store littleTIP height in a variable for further usage
+	// elT.mousemove(function(e){							// Function applied when mouse moves
+	// 	var BwiWi = $(window).width();					// Store window width in a variable for further usage
+	// 	var BwiHe = $(window).height();					// Store window height in a variable for further usage
+	// 	var BwHs = $(window).scrollLeft();				// Store window horizontal space in a variable for further usage
+	// 	var BwVs = $(window).scrollTop();				// Store window vertical space in a variable for further usage
+	// 	var liTIP = $('#littleTIP');					// Store littleTIP object in a variable for further usage
+	// 	var lTwi = $('#littleTIP').width();				// Store littleTIP width in a variable for further usage
+	// 	var lThe = $('#littleTIP').height();			// Store littleTIP height in a variable for further usage
 
-		if (BwiWi + BwHs < e.pageX + lTwi + littleTIPx)	// If window width and window horizontal space is small move littleTIP to the other side
-			{
-			liTIP.css("left", e.pageX - lTwi - littleTIPx);
-			}
-		else
-			{
-			liTIP.css("left", e.pageX + littleTIPx);
-			}
-		if (BwiHe + BwVs < e.pageY + lThe + littleTIPy)	// If window height and window vertical space is small move littleTIP to the other side
-			{
-			liTIP.css("top", e.pageY - lThe - littleTIPy);
-			}
-		else
-			{
-			liTIP.css("top", e.pageY + littleTIPy);
-			}
-	});
+	// 	if (BwiWi + BwHs < e.pageX + lTwi + littleTIPx)	// If window width and window horizontal space is small move littleTIP to the other side
+	// 		{
+	// 		liTIP.css("left", e.pageX - lTwi - littleTIPx);
+	// 		}
+	// 	else
+	// 		{
+	// 		liTIP.css("left", e.pageX + littleTIPx);
+	// 		}
+	// 	if (BwiHe + BwVs < e.pageY + lThe + littleTIPy)	// If window height and window vertical space is small move littleTIP to the other side
+	// 		{
+	// 		liTIP.css("top", e.pageY - lThe - littleTIPy);
+	// 		}
+	// 	else
+	// 		{
+	// 		liTIP.css("top", e.pageY + littleTIPy);
+	// 		}
+	// });
 
 	elT.mouseout(function(){			// Function applied when mouse moves out
 		$('#littleTIP').remove();		// Remove littleTIP
