e6d0b072725f081aa64fc1aafb13794695e4cb7d
Fenchel <msfenchel@gmail.com>
2013-02-06 11:48:28 -0600

    mobile rsvp

--- app/controllers/api/v2/rsvps_controller.rb
+++ app/controllers/api/v2/rsvps_controller.rb
@@ -8,17 +8,20 @@ class Api::V2::RsvpsController < ApplicationController
     @inout = params[:inout]
     if @mobile_user.nil?
       render :status=>400, :json=>{:error => "user was not found."}
+      return
     end
     if @event.nil?
       render :status=>400, :json=>{ :error => "event was not found."}
+      return
     end
-    if @inout == '0'
+    if @inout == 0
       @mobile_user.rsvp_out!(@event)
-    elsif @inout == '1'
+    elsif @inout == 1
       @mobile_user.rsvp_in!(@event)
       #rsvp in to parent idea
     else
       render :json=>{:error => "Trouble with server.. please try again :)"}
+      return
     end
     render :json=> { :success => true,
           :eid => @event.id
@@ -30,23 +33,33 @@ class Api::V2::RsvpsController < ApplicationController
     @mobile_user = User.find_by_id(params[:user_id])
     if @mobile_user.nil?
       render :status=>400, :json=>{:error => "user was not found."}
+      return
     end
     if @event.nil?
       render :status=>400, :json=>{:error => "event was not found."}
+      return
     end
     unless @mobile_user.rsvpd?(@event) == false
       @mobile_user.unrsvp!(@event)
     end
-        render :json=> { :success => true,
-          :eid => @event.id
-        }, :status=>200
+    render :json=> { :success => true,
+        :eid => @event.id
+      }, :status=>200
   end
 
   def flake_out
     @event = Event.find_by_id(params[:event_id])
     @mobile_user = User.find_by_id(params[:user_id])
+    if @mobile_user.nil?
+      render :status=>400, :json=>{:error => "user was not found."}
+      return
+    end
+    if @event.nil?
+      render :status=>400, :json=>{:error => "event was not found."}
+      return
+    end
     @mobile_user.flake_out!(@event)
-            render :json=> { :success => true,
+    render :json=> { :success => true,
                               :eid => @event.id
         }, :status=>200
   end
