5d94ff3875c2f4fc4b2c4158d973f2250314c7c5
ajbraus <ajbraus@gmail.com>
2013-02-18 15:31:44 -0600

    fixing out button

--- app/models/user.rb
+++ app/models/user.rb
@@ -231,7 +231,7 @@ class User < ActiveRecord::Base
     if event.parent.present?
       self.rsvp_in!(event.parent)
     else #contact only once if they sign up for time + idea 
-      unless event.user == self
+      unless event.user == self || event.over?
         event.user.delay.contact_new_rsvp(event, self)
       end
     end
