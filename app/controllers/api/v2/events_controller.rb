1929d5258faf6ba8218ef36d5285daba0f55d423
Fenchel <msfenchel@gmail.com>
2013-03-22 11:26:15 -0500

    fix API

--- app/controllers/api/v2/events_controller.rb
+++ app/controllers/api/v2/events_controller.rb
@@ -17,7 +17,7 @@ class Api::V2::EventsController < ApplicationController
     @finished = false
     @window_size = 7
 
-    @invites_ideas = Event.includes(:instances, {:rsvps => :guest}).where('city_id = ? AND ends_at IS NULL', @current_city.id).reject { |i| current_user.out?(i) || current_user.in?(i) || i.no_relevant_instances? }
+    @invites_ideas = @mobile_user.invited_ideas.where('city_id = ?', @current_city_id).order('created_at DESC').reject { |i| @mobile_user.out?(i) || i.no_relevant_instances? || @mobile_user.in?(i) }
 
     #ADAM's ATTEMPT AT THIS QUERY WITH NO SINGLETON ONE_TIMES AND WITH EAGER LOADING GUESTS AND INSTANCES
     # @invites_ideas = Event.includes(:instances, {:rsvps => :guest}).where('city_id = ? AND ends_at IS NULL', @current_city.id).reject { |e| e.no_relevant_instances? }
@@ -26,9 +26,9 @@ class Api::V2::EventsController < ApplicationController
     #   !current_user.in?(i) && (current_user.out?(i) || !current_user.invited?(i))
     # end
 
-    @invites_ideas = @invites_ideas.reject do |i| 
-      !current_user.in?(i) && (current_user.out?(i) || (current_user.inmates & i.guests).none? || (i.friends_only && !current_user.in?(i) && !i.user.is_friends_with?(current_user)))
-    end
+    # @invites_ideas = @invites_ideas.reject do |i| 
+    #   !current_user.in?(i) && (current_user.out?(i) || (current_user.inmates & i.guests).none? || (i.friends_only && !current_user.in?(i) && !i.user.is_friends_with?(current_user)))
+    # end
 
     # @invites_ideas = @invites_ideas.sort_by do |i| 
     #     i.guests.joins(:relationships).where('status = ? AND follower_id = ?', 2, current_user.id).count*1000 + 
