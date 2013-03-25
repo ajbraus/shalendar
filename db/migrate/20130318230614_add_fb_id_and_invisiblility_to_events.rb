123c089b671b60780db5330057bc0aaa0d195099
ajbraus <ajbraus@gmail.com>
2013-03-24 20:24:50 -0500

    create inviations

--- db/migrate/20130318230614_add_fb_id_and_invisiblility_to_events.rb
+++ db/migrate/20130318230614_add_fb_id_and_invisiblility_to_events.rb
@@ -1,15 +1,6 @@
 class AddFbIdAndInvisiblilityToEvents < ActiveRecord::Migration
   def change
-  	Event.all.each do |e|
-  		if e.friends_only?
-  			e.visibility = 1
-  		else
-  			e.visibility = 2
-  		end
-  		e.save
-  	end
 
-    remove_column :events, :friends_only
 
   end
 end
